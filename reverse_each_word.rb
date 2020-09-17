536e9910493d505adb81f52c3ca5bd1ec3b1a287
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-17 04:13:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,16 @@
 def reverse_each_word(str)
-  new_arr = []
-  str.split(" ").each{ |word|
-    new_word=[]
-    word.each_char { |letter|
-      new_word.unshift(letter)
-    }
-    new_arr.push(new_word.join())
-  }
-  return new_arr.join(" ")
+  # new_arr = []
+  # str.split(" ").each{ |word|
+  #   new_word=[]
+  #   word.each_char { |letter|
+  #     new_word.unshift(letter)
+  #   }
+  #   new_arr.push(new_word.join())
+  # }
+  # return new_arr.join(" ")
+
+  str.collect { |e|
+  puts e  }
 end
 
 # reverse_each_word("asdf")
