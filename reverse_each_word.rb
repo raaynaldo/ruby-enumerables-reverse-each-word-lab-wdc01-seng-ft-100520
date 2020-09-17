2f212f0d458a3fb919ec4b602b321360d48db498
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-17 04:16:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,4 @@
 def reverse_each_word(str)
-  # new_arr = []
-  # str.split(" ").each{ |word|
-  #   new_word=[]
-  #   word.each_char { |letter|
-  #     new_word.unshift(letter)
-  #   }
-  #   new_arr.push(new_word.join())
-  # }
-  # return new_arr.join(" ")
-
   str.split(" ").collect { |e|
     e.reverse
   }.join(" ")
